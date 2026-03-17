135c039f232f78809a2a4ebc59255ed0fce402c2
Yunaaaard <tarimanleonard28@gmail.com>
2026-03-17 11:02:06 +0800

    backup: snapshot current workspace changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+﻿
 import 'dart:io' show Platform;
+import 'dart:async';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:user_attendance_scanner/views/dashboard_page.dart';
 import 'package:user_attendance_scanner/views/home_page.dart';
 
 Future<void> main() async {
@@ -16,11 +20,22 @@ Future<void> main() async {
           DeviceOrientation.landscapeLeft,
           DeviceOrientation.landscapeRight,
         ]);
+      } else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+        SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+        await SystemChrome.setPreferredOrientations([
+          DeviceOrientation.landscapeLeft,
+          DeviceOrientation.landscapeRight,
+        ]);
       }
     } catch (_) {}
   }
 
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: kDebugMode,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -29,13 +44,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       title: 'FAST Attendance',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
+      // Keep loading screen visible while testing UI.
       home: const HomePage(),
+      // home: LoadingPage(loadFuture: Completer<void>().future),
     );
   }
 }
--- lib/views/dashboard_page.dart
+++ lib/views/dashboard_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
